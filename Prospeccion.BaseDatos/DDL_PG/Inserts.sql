9e233fb7958f79a8b82c0d010c34b4788b63a5f4
juan carlos armenta bautista <karloz.arba@gmail.com>
2024-10-01 02:57:07 -0600

    Se agrega scaffold de pg Los script de pg se marcan con opción NONE en compilación caso contrario genera error de sintaxis al compilar

---
+++ Prospeccion.BaseDatos/DDL_PG/Inserts.sql
@@ -0,0 +1,39 @@
+﻿insert into puesto (nombre, descripcion)
+values 
+	('Gestor', 'Agente de ventas y cobranza'),
+	('Administrador', 'Administrador de ventas');
+
+
+
+insert into Colaborador (nombre, apellidos, CorreoElectronico, idpuesto)
+values 
+	('Ivan','Meza','carlos.armenta@intelix.mx', 2),
+	('Alicia','Ambrosio','carlos.armenta@intelix.mx', 1);
+
+
+	
+insert into ColaboradorUsuario (IdColaborador, usuario, clave)
+values
+	(1, 'imm','123456'),
+	(2, 'azz','123456');
+	
+
+insert into permiso(nombre, descripcion)
+values
+	('Actividades', 'Acciones de Venta'),
+	('Resultados', 'Posibles Resultados'),
+	('Personas', 'Directorio'),
+	('Gestores', 'Personal Ventas'),
+	('Gestiones', 'Esfuerzo de ventas');
+	
+
+
+insert into ColaboradorUsuarioPermiso (IdColaboradorUsuario, IdPermiso)
+values
+	(1, 1),
+	(1, 2),
+	(1, 3),
+	(1, 4),
+	(2, 3),
+	(2, 5);
+
